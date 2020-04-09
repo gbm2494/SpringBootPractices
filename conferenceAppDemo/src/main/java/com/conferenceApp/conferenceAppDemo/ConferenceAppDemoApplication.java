999e4cffce6da2d90d071f19d713aa4cf2644b2d
gaudy <gbm2494@hotmail.com>
2020-04-08 20:44:26 -0600

    conference app example for Pluralsight course

--- conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/ConferenceAppDemoApplication.java
+++ conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/ConferenceAppDemoApplication.java
@@ -2,11 +2,9 @@ package com.conferenceApp.conferenceAppDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//changes to create a WAR instead of a JAR (copy war file on tomcat)
-public class ConferenceAppDemoApplication extends SpringBootServletInitializer {
+public class ConferenceAppDemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceAppDemoApplication.class, args);
