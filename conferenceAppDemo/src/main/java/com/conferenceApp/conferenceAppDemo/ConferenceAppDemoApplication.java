9b3d2286c7be9bde0fd789fe25c93ffa7b5f2f9e
gaudy <gbm2494@hotmail.com>
2020-04-08 20:42:14 -0600

    conference app example for Pluralsight course

--- conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/ConferenceAppDemoApplication.java
+++ conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/ConferenceAppDemoApplication.java
@@ -2,9 +2,11 @@ package com.conferenceApp.conferenceAppDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceAppDemoApplication {
+//changes to create a WAR instead of a JAR (copy war file on tomcat)
+public class ConferenceAppDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceAppDemoApplication.class, args);
--- conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/config/PersistenceConfiguration.java
+++ conferenceAppDemo/src/main/java/com/conferenceApp/conferenceAppDemo/config/PersistenceConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 @Configuration
 public class PersistenceConfiguration {
