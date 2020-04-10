1fe98aeadee568cee23da5241e2114a7fabf9e2e
gaudy <gbm2494@hotmail.com>
2020-04-09 18:54:34 -0600

    Boilerplate for Spring Data course

---
+++ springDataCourse/src/test/java/com/springDataCourse/springDataCourse/SpringDataCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDataCourse.springDataCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
